37b25b670ad136f8e3ce625d93e36f59c651e61e
msingh <msingh@google.com>
2024-05-29 16:33:53 -0700

    enabled prefill_insert and decode

--- MaxText/inference-benchmark-dataset.py
+++ MaxText/inference-benchmark-dataset.py
@@ -20,7 +20,7 @@
 #  base_output_directory=gs://${USER}-maxtext-outputs max_prefill_predict_length=1024 max_target_length=2048 per_device_batch_size=2 quantization="" quantize_kvcache=False  \
 #  load_parameters_path=gs://inference-benchmarks/models/llama2-7b/2024-04-25-14-01/param-only-decode-ckpt-maxtext/checkpoints/0/items
 #
-# Enable profiler by adding arg --enable_profiler=True
+# Enable profiler by adding arg: enable_profiler=True
 # """
 
 
@@ -54,12 +54,16 @@ _AUTOREGRESSIVE = "autoregressive"
 _MSEC_PER_TOK = 'msec_per_token'
 _MSEC_PER_SEQ = 'msec_per_sequence'
 
+def get_quantiles(name, stats_array):
+  return f'{name} -  min: {min(stats_array)}, max: {max(stats_array)}, deciles:{[round(s, 2) for s in statistics.quantiles(stats_array, n=10)]}'
+
 
 def read_openorca_dataset(filepath, print_stats=True):
   assert os.path.exists(filepath), f'input dataset file: {filepath}  does not exist'
+  # Read file
   with open(filepath) as f:
     dataset = json.load(f)
-
+  # Print stats
   if print_stats:
     n = len(dataset)
     len_prompt_tokens = []
@@ -71,108 +75,106 @@ def read_openorca_dataset(filepath, print_stats=True):
       len_total_tokens.append(d['len_prompt_tokens'] + d['len_output_tokens'])
     print(f'\nStats for dataset in file: {filepath}')
     print(f'Num requests: {n}')
-    max_len = max(len_prompt_tokens)
-    min_len = min(len_prompt_tokens)
-    quantiles = statistics.quantiles(len_prompt_tokens, n=10)
-    print(f'Distribution len prompt tokens: max: {max_len}, min: {min_len}, deciles: {quantiles}')
-    max_len = max(len_output_tokens)
-    min_len = min(len_output_tokens)
-    quantiles = statistics.quantiles(len_output_tokens, n=10)
-    print(f'Distribution len output tokens: max: {max_len}, min {min_len}, deciles: {quantiles}')
-    max_len = max(len_total_tokens)
-    min_len = min(len_total_tokens)
-    quantiles = statistics.quantiles(len_total_tokens, n=10)
-    print(f'Distribution len total tokens: max: {max_len}, min {min_len}, deciles: {quantiles}')
+    print(get_quantiles('len_prompt_tokens', len_prompt_tokens))
+    print(get_quantiles('len_output_tokens', len_output_tokens))
+    print(get_quantiles('len_total_tokens', len_total_tokens))
     print('\n')
   return dataset
 
-def tokenize_dataset(engine, dataset, prefill_length):
-  tokenized_dataset = []
-  metadata = engine.get_tokenizer()
-  vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
-  for i in range(len(dataset)):
-    datarow = dataset[i]
-    tokens, true_length = token_utils.tokenize_and_pad(
-        datarow['prompt'], vocab, is_bos=True, prefill_lengths=[prefill_length])
-    # Diff of 1 token because of bos prepended to list of tokens.
-    prompt_token_len =  datarow['len_prompt_tokens'] + 1
-    assert true_length == prompt_token_len, f'{true_length=} mismatches {prompt_token_len=}'
-    tokenized_dataset.append((tokens, true_length))
-  return tokenized_dataset
 
-
-def benchmark_prefill(config, engine, params, tokenized_data, warmup):
+def benchmark_prefill(config, engine, params, tokens, true_lengths, start_row):
   """Benchmarking prefill step."""
-  if not warmup:
-    max_utils.activate_profiler(config, _PREFILL)
+  batch_size = len(tokens)
+  # Warmup
+  if start_row == 0:
+    for i in range(_WARMUP_ITERS):
+      prefill_result = engine.prefill(params=params, padded_tokens=tokens[i], true_length=true_lengths[i])
+    jax.block_until_ready(prefill_result)
+    max_utils.delete_pytree(prefill_result)
+  # Benchmark
+  print("benchmark prefill")
+  max_utils.activate_profiler(config, f'{_PREFILL}_{start_row}')
   start = datetime.datetime.now()
-  for (tokens, true_length) in tokenized_data:
-    prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
+  for i in range(batch_size):
+    prefill_result = engine.prefill(params=params, padded_tokens=tokens[i], true_length=true_lengths[i])
   jax.block_until_ready(prefill_result)
   end = datetime.datetime.now()
   time_seconds = (end - start).total_seconds()
-  if not warmup:
-    max_utils.deactivate_profiler(config)
+  max_utils.deactivate_profiler(config)
   max_utils.delete_pytree(prefill_result)
+  # Stats
   stats = {
-    _MSEC_PER_SEQ: round(time_seconds*1000/len(tokenized_data), 2)
+    _MSEC_PER_SEQ: round(time_seconds*1000/batch_size, 2)
   }
   return stats
 
-def benchmark_prefill_insert(config, engine, params, decode_state, tokenized_data, warmup):
+def benchmark_prefill_insert(config, engine, decode_state, params, tokens, true_lengths, start_row):
   """Benchmarking prefill and insert step."""
-  if not warmup:
-    max_utils.activate_profiler(config, _PREFILL_INSERT)
-  start = datetime.datetime.now()
+  batch_size = len(tokens)
   total_slots = engine.max_concurrent_decodes
-  for i in range(len(tokenized_data)):
-    (tokens, true_length) = tokenized_data[i]
-    prefill_result = engine.prefill(params=params, padded_tokens=tokens, true_length=true_length)
+  # Warmup
+  if start_row == 0:
+    for i in range(_WARMUP_ITERS):
+      prefill_result = engine.prefill(params=params, padded_tokens=tokens[i], true_length=true_lengths[i])
+      decode_state = engine.insert(prefill_result, decode_state, int(i % total_slots))
+    jax.block_until_ready(decode_state)
+    max_utils.delete_pytree(prefill_result)
+  # Benchmark
+  print("benchmark prefill insert")
+  max_utils.activate_profiler(config, f'{_PREFILL_INSERT}_{start_row}:{start_row+batch_size}')
+  start = datetime.datetime.now()
+  for i in range(batch_size):
+    prefill_result = engine.prefill(params=params, padded_tokens=tokens[i], true_length=true_lengths[i])
     decode_state = engine.insert(prefill_result, decode_state, int(i % total_slots))
   jax.block_until_ready(decode_state)
   end = datetime.datetime.now()
   time_seconds = (end - start).total_seconds()
-  if not warmup:
-    max_utils.deactivate_profiler(config)
+  max_utils.deactivate_profiler(config)
   max_utils.delete_pytree(prefill_result)
+  # Stats
   stats = {
-    _MSEC_PER_SEQ: round(time_seconds*1000/len(tokenized_data), 2)
+    _MSEC_PER_SEQ: round(time_seconds*1000/batch_size, 2)
   }
   return stats, decode_state
 
-def benchmark_autoregressive(config, engine, params, decode_state, batch_size, warmup):
+def benchmark_autoregressive(config, engine, decode_state, params, batch_size, start_row):
   """Benchmarking autoregressive step."""
-  sampled_tokens_list = []
   steps = range(config.max_prefill_predict_length, config.max_target_length)
-  if not warmup:
-    max_utils.activate_profiler(config, _AUTOREGRESSIVE)
+  # Warmup
+  if start_row == 0:
+    for i in range(_WARMUP_ITERS):
+      decode_state, sampled_tokens = engine.generate(params, decode_state)
+    jax.block_until_ready(decode_state)
+  # Benchmark
+  print("benchmark autoregressive")
+  sampled_tokens_list = []
+  max_utils.activate_profiler(config, f'{_AUTOREGRESSIVE}_{start_row}:{start_row+batch_size}')
   start = datetime.datetime.now()
   for _ in steps:
     decode_state, sampled_tokens = engine.generate(params, decode_state)
     sampled_tokens_list.append(sampled_tokens)
   jax.block_until_ready(decode_state)
   end = datetime.datetime.now()
-  if not warmup:
-    max_utils.deactivate_profiler(config)
+  max_utils.deactivate_profiler(config)
+  max_utils.delete_pytree(decode_state)
   time_seconds = (end - start).total_seconds()
+  # Stats
   stats = {
-    _MSEC_PER_TOK: round(time_seconds*1000/(steps*batch_size), 2)
+    _MSEC_PER_TOK: round(time_seconds*1000/(len(steps)*batch_size), 2)
   }
   return stats, sampled_tokens_list
 
-def benchmark(config, engine, params, num_params, decode_state, tokenized_batch, warmup):
-    batch_size = len(tokenized_batch)
-    prefill_stats = benchmark_prefill(config, engine, params, tokenized_batch, warmup)
+def benchmark(config, engine, decode_state, params, tokens, true_lengths, suffix):
+    batch_size = len(tokens)
+    prefill_stats = benchmark_prefill(config, engine, params, tokens, true_lengths, suffix)
     prefill_insert_stats = None
-    #prefill_insert_stats, decode_state = benchmark_prefill_insert(config, engine, params, decode_state, tokenized_batch, warmup)
+    prefill_insert_stats, decode_state = benchmark_prefill_insert(config, engine, decode_state, params, tokens, true_lengths, suffix)
     ar_stats = None
     output_tokens = None
-    #ar_stats, output_tokens = benchmark_autoregressive(config, engine, params, decode_state, batch_size, warmup)
+    ar_stats, output_tokens = benchmark_autoregressive(config, engine, decode_state, params, batch_size, suffix)
     batch_stats = (prefill_stats, prefill_insert_stats, ar_stats)
     return batch_stats, output_tokens
 
-def get_quantiles(stats_array):
-  return f'min: {min(stats_array)}, max: {max(stats_array)}, deciles:{ round(s, 2) for s in statistics.quantiles(stats_array, n=10)}'
 
 def aggregate_stats(stats_list):
   output_stats = {}
@@ -190,7 +192,7 @@ def aggregate_stats(stats_list):
   result = {}
   for k in output_stats.keys():
     if len(output_stats[k]) > 0:
-      result[k] = get_quantiles(output_stats[k])
+      result[k] = get_quantiles(k, output_stats[k])
   return result
 
 
@@ -200,8 +202,6 @@ def main(config):
 
   # Set up initial state
   decode_state = engine.init_decode_state()
-  # cache_shape = jax.tree.map(lambda x: x.shape, decode_state["cache"])
-  # print(f"Cache shape {cache_shape}")
   _, cache_size, _ = max_utils.summarize_pytree_data(decode_state["cache"], name="Cache")
   num_params, model_size, _ = max_utils.summarize_pytree_data(params, name="Model")
   batch_size = engine.max_concurrent_decodes
@@ -209,30 +209,29 @@ def main(config):
   # Read the dataset
   dataset = read_openorca_dataset(_DATAFILE)
   dataset_size = len(dataset)
-  tokenized_dataset = tokenize_dataset(engine, dataset, _PREFILL_LENGTH)
-  assert len(tokenized_dataset) == dataset_size
-
-  # Warmup
-  print("Warmup started ..")
-  for _ in range(_WARMUP_ITERS):
-    e = min(batch_size, dataset_size)
-    tokenized_batch = tokenized_dataset[0:e]
-    # print(f'Warmup - batch (0:{e})')
-    # print([f'num_tokens:{len(t)}, true_len:{l}' for (t,l) in tokenized_batch])
-    # print()
-    _, _= benchmark(config, engine, params, num_params, decode_state, tokenized_batch, True)
+  metadata = engine.get_tokenizer()
+  vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
 
   # Benchmark
   stats = []
   print(f"\nBenchmarks started: dataset_size {dataset_size} and batch_size {batch_size}")
-  for s in range(0, dataset_size, batch_size):
-    e = min(s + batch_size, dataset_size)
-    tokenized_batch = tokenized_dataset[s:e]
-    # print(f'Benchmark - iter:({s}:{e})')
-    # print([f'num_tokens:{len(t)}, true_len:{l}' for (t,l) in tokenized_batch])
-    batch_stats, batch_output_tokens = benchmark(config, engine, params, num_params, decode_state, tokenized_batch, False)
-    # print(f" batch ({s}:{e}), batch_stats {batch_stats}")
+  for start_row in range(0, dataset_size, batch_size):
+    decode_state = engine.init_decode_state()
+    end_row = min(start_row + batch_size, dataset_size)
+    prefill_tokens = {}
+    prefill_true_lengths = {}
+    count = 0
+    for j in range(start_row, end_row):
+      text = dataset[j]['prompt']
+      prefill_tokens[count], prefill_true_lengths[count] = token_utils.tokenize_and_pad(
+        text, vocab, is_bos=True, prefill_lengths=[_PREFILL_LENGTH])
+      if count == 0:
+        print(f'\nProcessing batch of size {end_row-start_row} consisting of rows {start_row} to {end_row}')
+        #print(f'Sample prompt: {text}')
+      count+=1
+    batch_stats, batch_output_tokens = benchmark(config, engine, decode_state, params, prefill_tokens, prefill_true_lengths, start_row)
     stats.append(batch_stats)
+    #print(stats)
 
   # Aggregate stats
   print('\nAggregating stats')
@@ -241,8 +240,7 @@ def main(config):
   # Output stats
   print('\nResults')
   for k in result.keys():
-    print('\t' + f'{k}: {result[k]}')
-
+    print('\t' + f'{result[k]}')
 
   print('\nEnd')
   # TBD - add rouge score
