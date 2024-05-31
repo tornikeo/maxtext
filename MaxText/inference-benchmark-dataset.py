6814f58ec18e0d6053cb2842845bd44265b4e84a
msingh <msingh@google.com>
2024-05-30 18:01:30 -0700

    Adding rouge score to benchmarking script

--- MaxText/inference-benchmark-dataset.py
+++ MaxText/inference-benchmark-dataset.py
@@ -23,12 +23,16 @@
 # Enable profiler by adding arg: enable_profiler=True
 # """
 
-
+# pip install nltk
+# pip install rouge_score
+# pip install evaluate
 
 """Inference microbenchmark for prefill and autoregressive steps."""
 import datetime
+import evaluate
 import jax
 import json
+import numpy as np
 import statistics
 import sys
 
@@ -37,25 +41,62 @@ from jetstream.engine import token_utils
 import max_utils
 import maxengine
 import maxtext_utils
+import nltk
+
 import pyconfig
 import os
 
-_WARMUP_ITERS = 2 # Number of batches in dataset used for warmup
+_WARMUP_ITERS = 1 # Number of batches in dataset used for warmup
 _PREFILL_LENGTH = 1024 # For padding input tokens
 
-_NUM_SAMPLES=100
+_NUM_SAMPLES=24000
+#_NUM_SAMPLES=10
 _DATAFILE = f"/home/msingh/datasets/openorca_json/output_{_NUM_SAMPLES}.json"
-_OUTFILE=f"/home/msingh/benchmarks/openorca_json/rouge_{_NUM_SAMPLES}.json"
 
 _PREFILL = "prefill"
 _PREFILL_INSERT = "prefill_insert"
 _AUTOREGRESSIVE = "autoregressive"
 
+_MSEC_PER_SEQ_PER_TOK = 'msec_per_seq_per_token'
 _MSEC_PER_TOK = 'msec_per_token'
 _MSEC_PER_SEQ = 'msec_per_sequence'
+_TOKENS_PER_SEC = 'tokens/sec'
 
+_PRINT_BATCH_STATS = False
 def get_quantiles(name, stats_array):
   return f'{name} -  min: {min(stats_array)}, max: {max(stats_array)}, deciles:{[round(s, 2) for s in statistics.quantiles(stats_array, n=10)]}'
+
+def postprocess_text(preds, targets):
+  preds = [pred.strip() for pred in preds]
+  targets = [target.strip() for target in targets]
+  # rougeLSum expects newline after each sentence
+  preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+  targets = ["\n".join(nltk.sent_tokenize(target)) for target in targets]
+  return preds, targets
+
+
+def eval_accuracy(predicted, target):
+  assert len(predicted) == len(target), 'Mismatched predicted and target outputs.'
+  metric = evaluate.load("rouge")
+  nltk.download("punkt")
+  preds = []
+  targets = []
+
+  preds, targets = postprocess_text(predicted, target)
+  result = metric.compute(
+      predictions=preds,
+      references=targets,
+      use_stemmer=True,
+      use_aggregator=False,
+  )
+  result = {k: float(round(np.mean(v) * 100, 4)) for k, v in result.items()}
+  prediction_lens = [len(pred) for pred in preds]
+  target_lens = [len(tgt) for tgt in targets]
+  result["num_requests"] = len(preds)
+  result["sum_len_preds"] = int(np.sum(prediction_lens))
+  result["sum_len_targets"] = int(np.sum(target_lens))
+  result["num_requests"] = len(preds)
+  return result
 
 
 def read_openorca_dataset(filepath, print_stats=True):
@@ -131,7 +172,7 @@ def benchmark_prefill_insert(config, engine, decode_state, params, tokens, true_
   time_seconds = (end - start).total_seconds()
   max_utils.deactivate_profiler(config)
   max_utils.delete_pytree(prefill_result)
-  # Stats
+
   stats = {
     _MSEC_PER_SEQ: round(time_seconds*1000/batch_size, 2)
   }
@@ -160,12 +201,15 @@ def benchmark_autoregressive(config, engine, decode_state, params, batch_size, s
   time_seconds = (end - start).total_seconds()
   # Stats
   stats = {
-    _MSEC_PER_TOK: round(time_seconds*1000/(len(steps)*batch_size), 2)
+    _MSEC_PER_SEQ_PER_TOK: round(time_seconds*1000/(len(steps)), 2),
+    _MSEC_PER_TOK: round(time_seconds*1000/(len(steps)*batch_size), 2),
+    _TOKENS_PER_SEC: round(batch_size*len(steps)/time_seconds, 2)
   }
   return stats, sampled_tokens_list
 
 def benchmark(config, engine, decode_state, params, tokens, true_lengths, suffix):
     batch_size = len(tokens)
+    prefill_stats = None
     prefill_stats = benchmark_prefill(config, engine, params, tokens, true_lengths, suffix)
     prefill_insert_stats = None
     prefill_insert_stats, decode_state = benchmark_prefill_insert(config, engine, decode_state, params, tokens, true_lengths, suffix)
@@ -181,6 +225,7 @@ def aggregate_stats(stats_list):
   output_stats[f'{_PREFILL}-{_MSEC_PER_SEQ}'] = []
   output_stats[f'{_PREFILL_INSERT}-{_MSEC_PER_SEQ}'] = []
   output_stats[f'{_AUTOREGRESSIVE}-{_MSEC_PER_TOK}'] = []
+  output_stats[f'{_AUTOREGRESSIVE}-{_MSEC_PER_SEQ_PER_TOK}'] = []
   for (prefill_stats, prefill_insert_stats, ar_stats) in stats_list:
     if prefill_stats and _MSEC_PER_SEQ in prefill_stats:
       output_stats[f'{_PREFILL}-{_MSEC_PER_SEQ}'].append(prefill_stats[_MSEC_PER_SEQ])
@@ -188,13 +233,22 @@ def aggregate_stats(stats_list):
       output_stats[f'{_PREFILL_INSERT}-{_MSEC_PER_SEQ}'].append(prefill_insert_stats[_MSEC_PER_SEQ])
     if ar_stats and _MSEC_PER_TOK in ar_stats:
       output_stats[f'{_AUTOREGRESSIVE}-{_MSEC_PER_TOK}'].append(ar_stats[_MSEC_PER_TOK])
-
+      output_stats[f'{_AUTOREGRESSIVE}-{_MSEC_PER_SEQ_PER_TOK}'].append(ar_stats[_MSEC_PER_SEQ_PER_TOK])
+  # Result
   result = {}
   for k in output_stats.keys():
-    if len(output_stats[k]) > 0:
+    if output_stats[k] and len(output_stats[k]) > 1:
       result[k] = get_quantiles(k, output_stats[k])
   return result
 
+def tokens_to_text(tokenizer, sampled_tokens_list, batch_size):
+  text_output = []
+  for slot in range(batch_size):
+    results = [sampled_tokens.get_result_at_slot(slot).tokens.item() for sampled_tokens in sampled_tokens_list]
+    text = tokenizer.detokenize(results)
+    # print(f"slot {slot} text:\n{text}")
+    text_output.append(text)
+  return text_output
 
 def main(config):
   engine = maxengine.MaxEngine(config)
@@ -214,6 +268,7 @@ def main(config):
 
   # Benchmark
   stats = []
+  output_text = []
   print(f"\nBenchmarks started: dataset_size {dataset_size} and batch_size {batch_size}")
   for start_row in range(0, dataset_size, batch_size):
     decode_state = engine.init_decode_state()
@@ -223,15 +278,25 @@ def main(config):
     count = 0
     for j in range(start_row, end_row):
       text = dataset[j]['prompt']
+      batch_size = end_row - start_row
       prefill_tokens[count], prefill_true_lengths[count] = token_utils.tokenize_and_pad(
         text, vocab, is_bos=True, prefill_lengths=[_PREFILL_LENGTH])
       if count == 0:
-        print(f'\nProcessing batch of size {end_row-start_row} consisting of rows {start_row} to {end_row}')
+        print(f'\nProcessing batch of size {batch_size} consisting of rows {start_row} to {end_row}')
         #print(f'Sample prompt: {text}')
       count+=1
-    batch_stats, batch_output_tokens = benchmark(config, engine, decode_state, params, prefill_tokens, prefill_true_lengths, start_row)
+    batch_stats, output_tokens = benchmark(config, engine, decode_state, params, prefill_tokens, prefill_true_lengths, start_row)
+    batch_output_text = tokens_to_text(vocab.tokenizer, output_tokens, batch_size)
+    rouge_val = eval_accuracy(batch_output_text, [dataset[j]['output'] for j in range(start_row, end_row)])
+    output_text.extend(batch_output_text)
     stats.append(batch_stats)
-    #print(stats)
+    if _PRINT_BATCH_STATS:
+      (prefill_stats, prefill_insert_stats, ar_stats) = batch_stats
+      print(f'\nPrefill: {prefill_stats}')
+      print(f'\nPrefill-Insert: {prefill_insert_stats}')
+      print(f'\nAutoregressive: {ar_stats}')
+      print(f'\nAccuracy: {rouge_val}')
+
 
   # Aggregate stats
   print('\nAggregating stats')
@@ -241,6 +306,11 @@ def main(config):
   print('\nResults')
   for k in result.keys():
     print('\t' + f'{result[k]}')
+
+  # Output overall rouge score
+  rouge_val = eval_accuracy(output_text, [dataset[j]['output'] for j in range(len(dataset))])
+  print(f'\nAccuracy: {rouge_val}')
+
 
   print('\nEnd')
   # TBD - add rouge score
