9567b63c428fbbd0a594fb29de5ba9242460aefd
Mohit Khatwani <mohitkhatwani@google.com>
2024-12-20 20:54:08 +0000

    golden script changes

--- MaxText/scratch_code/golden_llama3-70b_export.py
+++ MaxText/scratch_code/golden_llama3-70b_export.py
@@ -17,51 +17,12 @@ Usage: python3 golden_llama3-70b_export.py --model-id meta-llama/Meta-Llama-3-70
 """
 
 import torch
+import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import jsonlines
 from google.cloud import storage
 
 # Load the tokenizer and model from Hugging Face
-
-model_id = "meta-llama/Llama-3.1-8B"
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-    "meta-llama/Llama-3.1-8B",
-    torch_dtype=torch.float32,
-)
-
-
-# Your prompt text
-prompt_texts = ["I love to"]
-all_data_to_save = []
-
-output_path = "golden_data_orig_llama3_1_8b.jsonl"
-
-
-for prompt_text in prompt_texts:
-  # Encode the prompt text
-  input_ids = tokenizer.encode(prompt_text, return_tensors="pt")
-
-  # Get the logits for the prompt + completion
-  with torch.no_grad():
-    outputs = model(input_ids)
-    logits = outputs.logits
-
-    # Convert logits to fp32
-    logits = logits.cpu().numpy().astype("float32")
-
-    # Prepare data to be saved
-    data_to_save = {
-        "prompt": prompt_text,
-        "tokens": input_ids.tolist()[0],
-        "logits": logits.tolist()[0],  # Convert numpy array to list for JSON serialization
-    }
-    all_data_to_save.append(data_to_save)
-
-with jsonlines.open(output_path, "w") as f:
-  f.write_all(all_data_to_save)
-
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
   """Uploads a file to the bucket."""
@@ -71,6 +32,51 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
   blob.upload_from_filename(source_file_name)
 
+def save_golden_logits(model_id, output_path):
+  tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B")
+  model = AutoModelForCausalLM.from_pretrained(
+      model_id,
+      torch_dtype=torch.float32,
+  )
 
-upload_blob("maxtext-llama", output_path, "llama3.1_8b/golden-logits/" + output_path)
-print("File {} uploaded to {}.".format(output_path, "llama3.1_8b/golden-logits/" + output_path))
+  # Your prompt text
+  prompt_texts = ["I love to"]
+  all_data_to_save = []
+
+
+  for prompt_text in prompt_texts:
+    # Encode the prompt text
+    input_ids = tokenizer.encode(prompt_text, return_tensors="pt")
+
+    # Get the logits for the prompt + completion
+    with torch.no_grad():
+      outputs = model(input_ids)
+      logits = outputs.logits
+
+      # Convert logits to fp32
+      logits = logits.cpu().numpy().astype("float32")
+
+      # Prepare data to be saved
+      data_to_save = {
+          "prompt": prompt_text,
+          "tokens": input_ids.tolist()[0],
+          "logits": logits.tolist()[0],  # Convert numpy array to list for JSON serialization
+      }
+      all_data_to_save.append(data_to_save)
+
+  with jsonlines.open(output_path, "w") as f:
+    f.write_all(all_data_to_save)
+
+  upload_blob("maxtext-llama", output_path, "Llama3_1_8B/golden-logits/" + output_path)
+  print("File {} uploaded to {}.".format(output_path, "Llama3_1_8B/golden-logits/" + output_path))
+
+def main(raw_args=None) -> None:
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--model-id", type=str, required=False, default='meta-llama/Llama-3.1-8B')
+  parser.add_argument("--output-path", type=str, required=True)
+  args = parser.parse_args(raw_args)
+  save_golden_logits(args.model_id, args.output_path)
+
+
+if __name__ == "__main__":
+  main()
